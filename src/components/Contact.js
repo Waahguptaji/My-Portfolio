d382b17a204a163889c2e1292c721be33bc10960
Sahil Gupta <sahilgupta742@gmail.com>
2023-11-04 14:48:17 +0530

    updating in React

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,6 +10,25 @@ const Contact = () => {
     window.open(url, "_blank");
   };
 
+  const mobileNav = document.querySelector(".mobilenav");
+  const header = document.querySelector(".header");
+
+  //Here we are making a toggle using classList property.
+  //when the button is clicked, the classList.toggle() method is called on the button element.
+  // If the "active" class is already applied to the button, it will be removed. If it's not applied, it will be added.
+  toggle = () => {
+    header.classList.toggle("active");
+  };
+  //when click on button it will call the toggle function.
+  mobileNav.addEventListener("click", () => {
+    toggle();
+  });
+
+  function openContact() {
+    var url = "contact.html";
+    window.open(url);
+  }
+
   return (
     <div>
       {/* <div className="polygon-Background"></div> */}
